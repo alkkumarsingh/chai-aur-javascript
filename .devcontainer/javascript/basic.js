0effcff510b3ad4393cdc2af83e6224231b44de2
alkkumarsingh <100098090+alkkumarsingh@users.noreply.github.com>
2024-09-25 19:14:58 +0000

    datatypes

---
+++ .devcontainer/javascript/basic.js
@@ -0,0 +1,9 @@
+const accountid=122323
+let accountemail="alksi@gmail.com"
+var accountpass="123344"
+accountcity="banglore"
+
+//accountid=12322//
+
+console.log(accountid);
+
---
+++ .devcontainer/javascript/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//alert(3+3) we are using node js 
+
+let name = "alok"
+let age =19
+let islogin=false
+// 2 to the power 52 range 
+console.log(typeof "alok");
+
